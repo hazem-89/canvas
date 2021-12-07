883ee7cdba6054106e48f6f191a97e5f56e204ac
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-07 10:33:48 +0100

    training

--- main.js
+++ main.js
@@ -6,11 +6,39 @@ function main() {
     const context = canvas.getContext('2d');
 
     // chose color
-    context.fillStyle = 'red';
-    context.strokeStyle = 'black';
+    // context.fillStyle = 'red';
+    // context.strokeStyle = 'black';
 
     //
-    context.fillRect(25, 25, 100, 100);
-    context.clearRect(50 , 50, 50, 50);
-    context.strokeRect(60 , 60, 30, 30);
+    // context.fillRect(25, 25, 100, 100);
+    // context.clearRect(50 , 50, 50, 50);
+    context.strokeRect(215 , 155, 30, 30);
+    context.strokeRect(155 , 155, 30, 30);
+
+    context.beginPath();
+    context.moveTo(215, 170);
+    context.lineTo(185, 170);
+    context.stroke();
+
+    
+    context.fillStyle = 'green';
+    context.lineWidth = 5;
+
+    context.beginPath();
+    context.arc(200, 200, 40, 0, Math.PI);
+    context.stroke();
+
+    context.beginPath();
+    context.arc(200, 200, 80, 0, 2 * Math.PI );
+    context.stroke();
+
+    context.beginPath();
+    context.arc(230, 170, 10, 0, 2 * Math.PI );
+    context.stroke();
+
+
+    context.beginPath();
+    context.arc(170, 170, 10, 0, 2 * Math.PI );
+    context.stroke();
 }
+
